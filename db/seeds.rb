f4a39001d46fa3c44275ce316da803c141c060e2
Romain Thiriot <85564151+Romain-Th@users.noreply.github.com>
2021-08-17 10:21:44 +0200

    ModelKickstart jour 1 done (Devise +home page et index)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Transport.new(transport_type: 'Vélo', description: 'Super vélo bleu')
