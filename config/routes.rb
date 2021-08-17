63ae350b88c0e5d2a06d2fcad693978eeef8032c
Romain Thiriot <85564151+Romain-Th@users.noreply.github.com>
2021-08-17 11:43:47 +0200

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :transports
+  resources :transports, only: [:index, :show] do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations, only: [:index, :show]
+  namespace :owner do
+    resources :transports, only: [:index, :show, :create, :update, :destroy]
+  end
 end
