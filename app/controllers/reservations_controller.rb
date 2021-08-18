8032b690c95539e54a3d53bd95ed250bafb69600
robin chane <robin.chane@gmail.com>
2021-08-18 14:42:32 +0200

    added full reservation bouton + model + controller + view + validation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,29 +1,28 @@
 class ReservationsController < ApplicationController
 
   def index
-    @reservations = Reservation.all
+    @reservations = Reservation.where(user: current_user)
   end
 
   def show
     @reservation = Reservation.find(params[:id])
   end
 
-  def new
-    @reservation = Reservation.new
-  end
-
   def create
     @reservation = Reservation.new(reservation_params)
-    if @reservation.save
-      redirect_to reservation_path(@user)
+    @reservation.transport_id = params[:transport_id]
+    @reservation.user = current_user
+    if @reservation.valid?
+      @reservation.save
+      redirect_to reservations_path(current_user)
     else
-      render :new
+      redirect_to transport_path(params[:transport_id])
     end
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:user_id, :transport_id, :reservation_date)
+    params.require(:reservation).permit(:reservation_date)
   end
 end
--- app/controllers/transports_controller.rb
+++ app/controllers/transports_controller.rb
@@ -1,12 +1,12 @@
 class TransportsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  
+
   def index
     @transports = Transport.all
   end
 
   def show
     @transport = Transport.find(params[:id])
+    @reservation = Reservation.new
   end
-
 end
