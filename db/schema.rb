917447f3d0ccf2175c49b2ed9728bbcd5970bc1b
RobinTCH <78359136+RobinTCH@users.noreply.github.com>
2021-08-19 17:17:46 +0200

    new working seed (#35)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_08_19_090520) do
-
+ActiveRecord::Schema.define(version: 2021_08_19_105635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
