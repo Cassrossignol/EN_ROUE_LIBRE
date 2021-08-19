a8ab94d367bce623deddc5e96d03933998657e2a
Tevy Ung <isabelle.ung@kedgebs.com>
2021-08-19 13:08:24 +0200

    searchbar done

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,10 +1,9 @@
 class Transport < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
-
   include PgSearch::Model
   pg_search_scope :search_by_transport_type_and_description,
-    against: [ :transport_type, :description ],
+    against: [:username, :transport_type, :description ],
     using: {
       tsearch: { prefix: true }
     }
