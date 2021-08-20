889519c883b65a8128109b8e35e26d0792d32c32
Tevy Ung <isabelle.ung@kedgebs.com>
2021-08-20 12:37:27 +0200

    en cours de dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_105635) do
-
+ActiveRecord::Schema.define(version: 2021_08_19_152141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,10 +55,10 @@ ActiveRecord::Schema.define(version: 2021_08_19_105635) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.string "price"
     t.string "username"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_transports_on_user_id"
   end
 
