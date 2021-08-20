4312f58e5dbbb3d74ca4716cef0be4b5a242888b
Cassandre Rossignol <cassandre.rossignol@kedgebs.com>
2021-08-20 15:12:15 +0200

    cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_152141) do
+ActiveRecord::Schema.define(version: 2021_08_20_100224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,10 +55,9 @@ ActiveRecord::Schema.define(version: 2021_08_19_152141) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "price"
-    t.string "username"
     t.float "latitude"
     t.float "longitude"
+    t.string "price"
     t.index ["user_id"], name: "index_transports_on_user_id"
   end
 
@@ -70,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_152141) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
